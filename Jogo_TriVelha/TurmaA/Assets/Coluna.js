a873b90f5358c4013e3cbfd3d538dd58b50158d4
enricllagostera <enric.llagostera@gmail.com>
2014-07-29 14:22:45 -0300

    turma A - versao com fim de jogo

--- Jogo_TriVelha/TurmaA/Assets/Coluna.js
+++ Jogo_TriVelha/TurmaA/Assets/Coluna.js
@@ -22,27 +22,35 @@ function OnMouseExit () {
 }
 
 function OnMouseUp () {
-	Debug.Log("clicou na coluna " + indice);
-	var linha = tabuleiro.TemEspacoLivre(indice);
-	print("linah com espaco livre: " + linha);
-	if (linha < 0) {
-		print("Coluna esta cheia");
-	}
-	else {
-		tabuleiro.matriz[linha, indice] = arbitro.jogadorAtual;
-		var posPeca:Vector3 = new Vector3 (
-			transform.position.x,
-			transform.position.y + 6f,
-			0);
-		Instantiate(arbitro.prefabPeca, 
-			posPeca, Quaternion.identity);
-		
-		// avança o jogador atual
-		arbitro.jogadorAtual++;
-		if(arbitro.jogadorAtual == 4) {
-			arbitro.jogadorAtual = 1;
+	// soh faz o que esta aqui se nao for fim de jogo
+	if(!arbitro.fimDeJogo) {
+		Debug.Log("clicou na coluna " + indice);
+		var linha = tabuleiro.TemEspacoLivre(indice);
+		print("linah com espaco livre: " + linha);
+		if (linha < 0) {
+			print("Coluna esta cheia");
 		}
-		arbitro.msg.text = "Vez do jogador " + arbitro.jogadorAtual;
-		print(tabuleiro.VerificarFimDeJogo());
+		else {
+			tabuleiro.matriz[linha, indice] = arbitro.jogadorAtual;
+			var posPeca:Vector3 = new Vector3 (
+				transform.position.x,
+				transform.position.y + 6f,
+				0);
+			Instantiate(arbitro.prefabPeca, posPeca, Quaternion.identity);
+			
+			// avança o jogador atual
+			arbitro.jogadorAtual++;
+			if(arbitro.jogadorAtual == 4) {
+				arbitro.jogadorAtual = 1;
+			}
+			arbitro.msg.text = "Vez do jogador " + arbitro.jogadorAtual;
+			
+			// checa se eh fim de jogo e escreve mensagem se alguem ganhou
+			if(tabuleiro.VerificarFimDeJogo()){
+				arbitro.fimDeJogo = true;
+				arbitro.msg.text = "O jogador " + (arbitro.jogadorAtual-1) + " ganhou!";
+			}
+		}
 	}
+	
 }
