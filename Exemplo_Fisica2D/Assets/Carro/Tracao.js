bd16bf80226a29f0a5d619ad8bfa43bdfda7ee7d
enricllagostera <enric.llagostera@gmail.com>
2014-07-30 17:06:47 -0300

    fisica2d - cena carro

---
+++ Exemplo_Fisica2D/Assets/Carro/Tracao.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var forca : float = 100f;
+private var input : float;
+
+// funcao chamada a cada frame
+function Update () {
+	// calcula qual eh a velocidade derivada do input horizontal
+	input = Input.GetAxis("Horizontal") * forca;
+}
+
+// funcao chamada em intervalos regulares
+function FixedUpdate() {
+	// altera o motor da HingeJoint2D para ter a velocidade de input
+	GetComponent.<HingeJoint2D>().motor.motorSpeed = input;
+}
