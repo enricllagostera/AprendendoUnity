785398e97c15e2c263b31a42e76ff8d50fb5d27f
enricllagostera <enric.llagostera@gmail.com>
2014-08-04 11:46:28 -0300

    fisica 2d - exemplo com camera que segue a caixa

--- Exemplo_Fisica2D/Assets/Aula_2014-08-04/SegueAlvo.js
+++ Exemplo_Fisica2D/Assets/Aula_2014-08-04/SegueAlvo.js
@@ -4,5 +4,10 @@ public var alvo : Transform;
 public var distancia : Vector3;
 
 function Update () {
-	transform.position = alvo.position + distancia;
+	// a posicao da camera recebe a posicao do alvo mais 
+	// a distancia que deve manter dele
+	// 1) versao sem suavizacao
+	// transform.position = alvo.position + distancia;
+	// 2) versao com suavizacao
+	transform.position = Vector3.Lerp (transform.position, alvo.position + distancia, Time.deltaTime);
 }
