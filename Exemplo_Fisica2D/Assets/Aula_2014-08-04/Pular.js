82a3f69d8d2c60b999fd6562cabf987bba9d23fa
enricllagostera <enric.llagostera@gmail.com>
2014-08-04 11:14:48 -0300

    fisica 2D - exemplo de sala de aula

---
+++ Exemplo_Fisica2D/Assets/Aula_2014-08-04/Pular.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var podePular : boolean;
+public var forcaPulo : float;
+
+function Update () {
+	if (Input.GetKeyDown (KeyCode.Space) && podePular) {
+		// soh acontece quando acabou de apertar espaco E (&&) podePular eh verdadeiro
+		rigidbody2D.AddForce (Vector3.up * rigidbody2D.mass * forcaPulo);
+		// aplica forca para cima (relativo ao mundo) : a forca ignora a massa
+		// e usa a variavel publica que podemos mudar no inspector
+	}
+}
+
+// funcao chamada quando comecou a colidir com algum objeto
+function OnCollisionEnter2D () {
+	// deixa pular
+	podePular = true;
+}
+
+// funcao chamada quando parou de colidir com algum objeto
+function OnCollisionExit2D () {
+	// proibe de pular
+	podePular = false;
+}
---
+++ Exemplo_Fisica2D/Assets/Aula_2014-08-04/SegueAlvo.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+public var alvo : Transform;
+public var distancia : Vector3;
+
+function Update () {
+	transform.position = alvo.position + distancia;
+}
