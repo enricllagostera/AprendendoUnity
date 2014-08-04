0d9cfadf970003d30a250abad45bd5f700692bce
enricllagostera <enric.llagostera@gmail.com>
2014-08-04 12:52:15 -0300

    fisica 3d - duas cenas

---
+++ Exemplo_Fisica3D/Assets/Mola/GerarCubos.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+public var cubo : Transform;
+
+function Update () {
+	if (Input.GetKeyDown (KeyCode.Space)) {
+		Instantiate (cubo, transform.position + new Vector3 (0, 12, 0), Quaternion.identity);
+	}
+}
