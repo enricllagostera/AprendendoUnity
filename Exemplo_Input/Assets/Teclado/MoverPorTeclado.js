9bb088a386e2d3c48282fcf65b0ea5a5c3acb358
enricllagostera <enric.llagostera@gmail.com>
2014-08-09 17:52:57 -0300

    exemplo de input - teclado

---
+++ Exemplo_Input/Assets/Teclado/MoverPorTeclado.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+// controles configuraveis no editor
+public var tCima : KeyCode = KeyCode.UpArrow;
+public var tBaixo : KeyCode = KeyCode.DownArrow;
+public var tEsquerda : KeyCode = KeyCode.LeftArrow;
+public var tDireita : KeyCode = KeyCode.RightArrow;
+// valor para escalar o deslocamento
+public var velocidade : float;
+// deslocamento por frame
+public var deslocamento : Vector3;
+
+function Update () {
+	// zera o deslocamento desse frame
+	deslocamento = Vector3.zero;
+	if (Input.GetKey (tCima)) {
+		// se a tecla cima estiver pressionada, aumenta o z do deslocamento
+		deslocamento.z += 1;
+	}
+	if (Input.GetKey (tBaixo)) {
+		deslocamento.z += -1;
+	}
+	if (Input.GetKey (tEsquerda)) {
+		deslocamento.x += -1;
+	}
+	if (Input.GetKey (tDireita)) {
+		deslocamento.x += 1;
+	}
+	// converte o deslocamento para ter modulo 1 e
+	// entao escala ele pela velocidade desejada, evitando
+	// que va mais rapido na diagonal
+	deslocamento = deslocamento.normalized * velocidade;
+}
+
+function FixedUpdate () {
+	// aplica o movimento movendo o rigidbody, para afetar 
+	// outros objetos fisicos corretamente
+	rigidbody.MovePosition (rigidbody.position + deslocamento * Time.fixedDeltaTime);
+}
---
+++ Exemplo_Input/Assets/Teclado/TrocarCorPorTeclado.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+// uma cor para cada estado da tecla
+public var corPressionou : Color;
+public var corPressionado : Color;
+public var corSoltou : Color;
+public var corSolto : Color;
+
+// a cada frame
+function Update () {
+	
+	// se nao estiver pressionado
+	if (!Input.GetKey (KeyCode.Space)) {
+		// muda a cor do material do renderer para solto
+		renderer.material.color = corSolto;
+		if (Input.GetKeyUp (KeyCode.Space)) {
+			// se acabou de soltar, muda a cor novamente
+			print ("Soltou");
+			renderer.material.color = corSoltou;
+		}
+		else {
+			// se jah estava solto, escreve uma mensagem
+			print ("Solto");
+		}
+	}
+	else {
+		// se estiver pressionado, muda cor
+		renderer.material.color = corPressionado;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// se acabou de pressionar
+			print ("Pressionou");
+			renderer.material.color = corPressionou;
+		}
+		else {
+			// se jah estava pressionado, escreve msg
+			print ("Pressionado");
+		}
+	}
+}
