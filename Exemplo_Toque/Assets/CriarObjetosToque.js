5c61cb5aa154361e41842959ad490437b4ab8eca
enricllagostera <enricllagostera@gmail.com>
2014-08-13 08:59:19 -0300

    xemplo de toque

---
+++ Exemplo_Toque/Assets/CriarObjetosToque.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+public var prefabObj : Transform;
+
+public var caixas : Dictionary.<int, Transform>;
+
+function Start () {
+	caixas = new Dictionary.<int, Transform>();
+}
+
+function Update () {
+	
+	if (Input.touchCount > 0) {
+		//print ("toques " + Input.touchCount);
+		for	(var i = 0; i < Input.touchCount; i++) {
+			// pega toque e calcula sua posicao no mundo
+			var toque : Touch = Input.GetTouch(i);
+			var posicao : Vector3 = Camera.main.ScreenToWorldPoint(toque.position);
+			posicao.z = 0;
+			// se jah tem esse dedo tocando
+			if (caixas.ContainsKey(toque.fingerId)) {
+				if (toque.phase == TouchPhase.Canceled ||
+					toque.phase == TouchPhase.Ended ) {
+					print ("saiu");
+					Destroy(caixas[toque.fingerId].gameObject);
+					caixas.Remove(toque.fingerId);
+				}
+				else {
+					print ("atualizou");
+					caixas[toque.fingerId].position = posicao;
+				}
+			}
+			else if (toque.phase == TouchPhase.Began) {
+				print ("criou");
+				caixas.Add(toque.fingerId, Instantiate (prefabObj, posicao, Quaternion.identity));
+			}
+		}
+		print ("caixas " + caixas.Count);
+	}
+	
+	
+}
---
+++ Exemplo_Toque/Assets/TestesFuncoes.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function OnMouseDown () {
+	print ("teste");
+}
