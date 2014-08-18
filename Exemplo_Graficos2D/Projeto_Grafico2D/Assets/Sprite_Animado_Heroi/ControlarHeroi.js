2f32fa2e435dff8bb69242a4c8870ce638644be5
enricllagostera <enricllagostera@gmail.com>
2014-08-18 11:47:22 -0300

    exemplo - aula 18-8

---
+++ Exemplo_Graficos2D/Projeto_Grafico2D/Assets/Sprite_Animado_Heroi/ControlarHeroi.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+private var animador : Animator;
+
+function Start () {
+	animador = GetComponent("Animator") as Animator;
+}
+
+function Update () {
+	if (Input.GetAxis("Horizontal") != 0) {
+		animador.SetBool("EstaCorrendo", true);
+	}
+	else {
+		animador.SetBool("EstaCorrendo", false);
+	}
+}
