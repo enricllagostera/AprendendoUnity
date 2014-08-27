6241b9d6ac7e7a820726a5acb1cc96603e919e00
enricllagostera <enricllagostera@gmail.com>
2014-08-27 08:59:54 -0300

    graficos 3d e nac 05

---
+++ Gabarito_NAC_05/Assets/DetectarChao.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+private var pai : MoverPersonagem;
+
+function Start () {
+	pai = transform.parent.GetComponent.<MoverPersonagem>();
+}
+
+function OnTriggerEnter2D () {
+	print ("entrou");
+	pai.estaNoChao = true;
+	pai.pulando = false;
+	pai.animator.SetTrigger("Levantando");
+}
+
+function OnTriggerExit2D () {
+	print ("descolou");
+	pai.estaNoChao = false;
+	
+}
---
+++ Gabarito_NAC_05/Assets/MoverPersonagem.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+public var aceleracaoX : float;
+public var velocidadeMaximaX : float;
+public var arrastoChao : float;
+public var animator : Animator;
+public var estaNoChao : boolean;
+public var velocidadePulo : float;
+public var tempoSubida : float;
+public var pulando : boolean;
+public var timer : float;
+public var modificadorAr : float;
+
+function Start () {
+	animator = GetComponentInChildren.<Animator>();
+}
+
+function Update () {
+	if (Input.GetButtonDown("Jump") && estaNoChao && !pulando) {
+		print ("entrou");
+		pulando = true;
+		timer = 0;
+		animator.SetTrigger("Pulando");
+	}
+}
+
+function FixedUpdate () {
+	
+	var aceleracaoReal = aceleracaoX;
+	
+	if (estaNoChao) {
+		rigidbody2D.drag = arrastoChao;
+	}
+	else {
+		rigidbody2D.drag = 0;
+		aceleracaoReal *= modificadorAr;
+	}
+	
+	if (Input.GetButton("Jump") && pulando && timer < tempoSubida) {
+		rigidbody2D.velocity.y = velocidadePulo;
+		timer += Time.fixedDeltaTime;
+	}
+	
+	// soh aplica aceleracao se a velocidade atual
+	// tiver modulo menor que a velocidade maxima
+	if (Mathf.Abs (rigidbody2D.velocity.x) < velocidadeMaximaX ) {
+		rigidbody2D.velocity.x += Input.GetAxis ("Horizontal") * aceleracaoReal * Time.fixedDeltaTime;
+	}
+	
+	// soh atualiza a direcao que o personagem olha quando
+	// tiver allgum input do jogador
+	if (Input.GetAxis ("Horizontal") != 0) {
+		transform.localScale.x = Mathf.Sign (Input.GetAxis ("Horizontal"));
+	}
+	
+	// calcula qual eh o coeficiente atual da velocidade em X 
+	// em relacao a velocidade maxima e passa isso para o Animator
+	var coeficiente = Mathf.Abs (rigidbody2D.velocity.x) / velocidadeMaximaX;
+	animator.SetFloat ("CoeficienteVelocidadeX", coeficiente); 
+}
