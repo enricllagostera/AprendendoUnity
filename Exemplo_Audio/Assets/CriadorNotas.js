9f55d5a99a4d1fcf47f477e6279fb164f9ed6d6d
enricllagostera <enricllagostera@gmail.com>
2014-09-02 00:12:43 -0300

    exemplo audio

---
+++ Exemplo_Audio/Assets/CriadorNotas.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+public var prefabNota : Transform;
+
+function Update () {
+	if (Input.GetMouseButtonUp(0)) {
+		var posMouse : Vector3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Instantiate (prefabNota, new Vector3 (posMouse.x, posMouse.y, 0), Quaternion.identity);
+	}
+}
---
+++ Exemplo_Audio/Assets/TocarSons.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var sons : AudioClip[];
+public var tocarClique : boolean = true;
+public var tocarColisao : boolean = false;
+
+function OnMouseDown () {
+	if (tocarClique) {
+		audio.PlayOneShot (sons [Random.Range (0, sons.Length)]);
+	}
+}
+
+function OnCollisionEnter2D (colisao : Collision2D) {
+	if (tocarColisao) {
+		audio.PlayOneShot (sons [Random.Range (0, sons.Length)], Mathf.Clamp01 (colisao.relativeVelocity.sqrMagnitude * 0.02f));
+	}
+}
