896ec15e9579669051a14e6886b90f376cbacf8e
enricllagostera <enricllagostera@gmail.com>
2014-09-10 08:32:28 -0300

    adicionei sons sincronizados com animation events

---
+++ Gabarito_NAC_05/Assets/SonsPersonagem.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var somPulo : AudioClip;
+public var somCair : AudioClip;
+
+function Pular () {
+	audio.PlayOneShot (somPulo);
+}
+
+function Cair () {
+	audio.PlayOneShot (somCair);
+}
