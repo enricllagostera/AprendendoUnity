8e38fe1b7c768564926c85c0e860bdec9c9656ba
enricllagostera <enricllagostera@gmail.com>
2014-09-22 07:39:45 -0300

    exemplo de textura animada

---
+++ Exemplo_TexturaAnimada/Assets/OffsetAnimado.js
@@ -0,0 +1,14 @@
+﻿ #pragma strict
+
+public var velocidadeX : float;
+public var velocidadeY : float;
+
+function Update () {
+	var offsetX = renderer.material.GetTextureOffset("_MainTex").x +
+		velocidadeX * Time.deltaTime;
+	var offsetY = renderer.material.GetTextureOffset("_MainTex").y +
+		velocidadeY * Time.deltaTime;
+	renderer.material.SetTextureOffset(
+		"_MainTex", 
+		new Vector2(offsetX, offsetY));
+}
