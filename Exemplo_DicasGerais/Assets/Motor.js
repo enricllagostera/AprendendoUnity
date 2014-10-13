44026aa2bac459564a161b6f5265c1d5352fc8e7
Enric Llagostera <enricllagostera@gmail.com>
2014-10-13 17:05:53 -0300

    Dicas gerais

---
+++ Exemplo_DicasGerais/Assets/Motor.js
@@ -0,0 +1,48 @@
+﻿#pragma strict
+
+public var movimento : float;
+public var direcao : Vector3;
+@RangeAttribute(0, 1) public var fatorAtrito : float;
+public var velocidadeMax : float;
+
+function Update () {
+	direcao = Vector3.zero;
+	if (Input.GetAxis("Vertical") > 0) {
+		direcao = new Vector3 (0, 0, 1);
+	}
+}
+
+function FixedUpdate () {
+	/*
+	rigidbody.AddForce (
+		direcao * movimento,
+		ForceMode.VelocityChange);
+	rigidbody.velocity = new Vector3 (
+		(direcao * movimento).x,
+		rigidbody.velocity.y,
+		(direcao * movimento).z);
+	*/
+	
+	var velDesejada : Vector3 = new Vector3 (
+		(direcao * movimento).x,
+		0,
+		(direcao * movimento).z);
+	
+	if ( rigidbody.velocity.magnitude < velocidadeMax) {	
+		rigidbody.velocity += velDesejada;
+	}
+	
+	var velEixos : Vector3 = new Vector3 (
+		rigidbody.velocity.x, 
+		0, 
+		rigidbody.velocity.z); 
+	
+	if ( velEixos.magnitude > 0 ) {
+		rigidbody.velocity = new Vector3 (
+			rigidbody.velocity.x * fatorAtrito,
+			rigidbody.velocity.y,
+			rigidbody.velocity.z * fatorAtrito);
+	}
+	
+	Debug.DrawRay (transform.position, transform.forward*5, Color.red);
+}
---
+++ Exemplo_DicasGerais/Assets/SeguirCaminho.js
@@ -0,0 +1,47 @@
+﻿#pragma strict
+
+public var pontos : Transform[];
+public var posAtual : int;
+public var posAlvo : int;
+public var velocidade : float;
+public var tolerancia : float;
+public var proximo : int;
+public var timer : float;
+
+function Start () {
+	posAtual = 0;
+	posAlvo = 1;
+	proximo = 1;
+	timer = 0;
+	transform.position = pontos[posAtual].position;
+}
+
+function Update () {
+
+	if (Input.GetKeyDown(KeyCode.W) ) {
+		proximo *= -1;
+	}
+
+
+	timer += Time.deltaTime;
+	
+	transform.position = Vector3.Lerp (
+		pontos[posAtual].position,
+		pontos[posAlvo].position,
+		timer * velocidade);
+	
+	transform.LookAt (pontos[posAlvo]);
+	
+	if (Vector3.Distance(
+			transform.position, 
+			pontos[posAlvo].position) < tolerancia ) {
+		timer = 0;
+		posAtual = posAlvo;
+		posAlvo += proximo;
+		
+		if (posAlvo == pontos.Length || posAlvo == -1) {
+			proximo *= -1;
+			posAlvo += proximo;
+		}
+	}
+}
